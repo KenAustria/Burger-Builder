28a1d22917e7f670f58134668cd42ee2506ee6ac
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-08-05 18:54:53 -0700

    Button Class Not Rendering

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -17,8 +18,12 @@ const orderSummary = props => {
       <p>Delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <button btnType="Danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </button>
+      <button btnType="Success" clicked={props.purchaseContinued}>
+        CONTINUE
+      </button>
     </Aux>
   );
 };
