50971ff4e745568e86ea5aa5ec70696560b083ef
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-08-21 18:39:58 -0700

    Handling Overall Form Validity

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,7 +18,9 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true
-        }
+        },
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -29,20 +31,24 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true
-        }
+        },
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
         elementConfig: {
           type: 'text',
-          placeholder: 'ZIP Code'
+          placeholder: 'Zip Code'
         },
         value: '',
         validation: {
           required: true,
           minLength: 5,
           maxLength: 5
-        }
+        },
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -53,18 +59,22 @@ class ContactData extends Component {
         value: '',
         validation: {
           required: true
-        }
+        },
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
         elementConfig: {
           type: 'email',
-          placeholder: 'Your E-Mail'
+          placeholder: 'Email'
         },
         value: '',
         validation: {
           required: true
-        }
+        },
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -75,11 +85,10 @@ class ContactData extends Component {
           ]
         },
         value: '',
-        validation: {
-          required: true
-        }
+        valid: true
       }
     },
+    formIsValid: false,
     loading: false
   };
 
@@ -109,19 +118,21 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
 
     if (rules.required) {
-      isValid = value.trip() !== '';
+      isValid = value.trim() !== '' && isValid;
     }
 
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
 
     if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength;
+      isValid = value.length <= rules.maxLength && isValid;
     }
+
+    return isValid;
   }
 
   inputChangedHandler = (event, inputIdentifier) => {
@@ -136,8 +147,14 @@ class ContactData extends Component {
       updatedFormElement.value,
       updatedFormElement.validation
     );
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
-    this.setState({ orderForm: updatedOrderForm });
+
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
   };
 
   render() {
@@ -156,10 +173,15 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={event => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>
+          ORDER
+        </Button>
       </form>
     );
     if (this.state.loading) {
