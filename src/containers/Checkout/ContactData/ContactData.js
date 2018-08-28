8a6e4d482422fa279aa6001f0861284c86320902
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-08-27 23:32:19 -0700

    Renamed purchaseBurgerStart action to purchaseBurger. Create new purchaseBurgerStart action

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -94,8 +94,7 @@ class ContactData extends Component {
         valid: true
       }
     },
-    formIsValid: false,
-    loading: false
+    formIsValid: false
   };
 
   orderHandler = event => {
@@ -195,7 +194,7 @@ class ContactData extends Component {
         </Button>
       </form>
     );
-    if (this.state.loading) {
+    if (this.props.loading) {
       form = <Spinner />;
     }
     return (
