c105513f3f497a26bbfc117bab278915dbee8103
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-08-28 13:30:02 -0700

    fetchOrdersStart, fetchOrdersFail, fetchOrdersSuccess

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -24,16 +24,32 @@ const reducer = (state = initialState, action) => {
         ...state,
         loading: false
       };
-    case actionTypes.PURCHASE_BURGER_START
+    case actionTypes.PURCHASE_BURGER_START:
       return {
         ...state,
         loading: true
-      }
-    case actionTypes.PURCHASE_INIT
+      };
+    case actionTypes.PURCHASE_INIT:
       return {
-        ...state
+        ...state,
         purchased: true
-      }
+      };
+    case actionTypes.FETCH_ORDERS_START:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionTypes.FETCH_ORDERS_SUCCESS:
+      return {
+        ...state,
+        orders: action.orders,
+        loading: false
+      };
+    case actionTypes.FETCH_ORDERS_FAIL:
+      return {
+        ...state,
+        loading: false
+      };
     case default:
       return state;
   }
