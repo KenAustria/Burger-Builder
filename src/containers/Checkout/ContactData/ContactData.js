1206d9706225549966ec8bb2b10146b8d676811a
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-08-27 20:55:02 -0700

    Created action-creators: purchaseBurgerSuccess, purchaseBurgerFail, purchaseBurgerStart

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -110,15 +110,6 @@ class ContactData extends Component {
       price: this.props.price,
       orderData: formData
     };
-    axios
-      .post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false });
-        this.props.history.push('/');
-      })
-      .catch(error => {
-        this.setState({ loading: false });
-      });
   };
 
   checkValidity(value, rules) {
@@ -220,4 +211,8 @@ const mapStateToProps = state => {
   };
 };
 
-export default connect(mapStateToProps)(ContactData);
+const mapDispatchToProps = dispatch => {
+  onOrderBurger: () => dispatch(actions.purchaseBurger());
+};
+
+export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
