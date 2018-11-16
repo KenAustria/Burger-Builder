f6b88d9a1e41acf3154b4418e8c7aa38eb94d9f5
kennpangilinan <28639365+KenAustria@users.noreply.github.com>
2018-11-15 18:53:07 -0800

    authLogout Reducer, checkAuthTimeout and logout action

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -16,3 +16,4 @@ export const PURCHASE_ORDERS_SUCCESS = 'PURCHASE_ORDERS_SUCCESS';
 export const AUTH_START = 'AUTH_START';
 export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAIL = 'AUTH_FAIL';
+export const AUTH_LOGOUT = 'AUTH_LOGOUT';
