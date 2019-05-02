fe97049dc44fdffd6313fcdb7648c71fef5a7c43
Ken Austria <kenaustriadev@gmail.com>
2019-05-02 12:34:05 -0700

    Token from Firebase and Added Sign-In

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -36,7 +36,8 @@ class Auth extends Component {
         valid: false,
         touched: false
       }
-    }
+		},
+		isSignup: true
 	};
 
 	checkValidity(value, rules) {
@@ -87,8 +88,15 @@ class Auth extends Component {
 		event.preventDefault();
 		this.props.onAuth(
 			this.state.controls.email.value,
-			this.state.controls.password.value
+			this.state.controls.password.value,
+			this.state.isSignup
 		)
+	}
+
+	switchAuthModeHandler = () => {
+		this.setState(prevState => {
+			return { isSignup: !prevState.isSignup }
+		});
 	}
 
   render() {
@@ -119,6 +127,9 @@ class Auth extends Component {
 					{form}
 					<Button btnType="Success">Submit</Button>
 				</form>
+				<Button btnType="Danger" clicked={this.switchAuthModeHandler}>
+					Switch to {this.state.isSignup ? 'Login' : 'Sign Up'}
+				</Button>
       </div>
     );
   }
@@ -126,7 +137,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
 	return {
-		onAuth: (email, password) => dispatch(actions.auth(email, password))
+		onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
 	};
 }
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,27 +1,47 @@
-import * as actionTypes from './actionTypes';
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
 
 export const authStart = () => {
-	return {
-		type: actionTypes.AUTH_START
-	};
-}
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
 
-export const authSuccess = (authData) => {
-	return {
-		type: actionTypes.AUTH_SUCCESS,
-		authData: authData
-	}
-}
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData
+  };
+};
 
-export const authFail = (error) => {
-	return {
-		type: actionTypes.AUTH_FAIL,
-		error: error
-	}
-}
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
 
-export const auth = (email, password) => {
-	return dispatch => {
-		dispatch(authStart());
+export const auth = (email, password, isSignup) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+		};
+		let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDa6bimv5aJ3Ta5wY3aX8NjW4pz-wtVcz8";
+		if (!isSignup) {
+			url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDa6bimv5aJ3Ta5wY3aX8NjW4pz-wtVcz8";
 	}
-}
+    axios
+      .post(url, authData)
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
